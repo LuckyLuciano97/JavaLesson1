c98185f394444ff4b88b22f8a53e87c53dd6fe46
LuckyLuciano97 <luka.djogas@gmail.com>
2025-01-22 16:17:21 +0100

    HW1 commit

--- JavaExample1/src/Main.java
+++ JavaExample1/src/Main.java
@@ -1,5 +1,4 @@
-import java.time.LocalDateTime;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.time.LocalDateTime;
 public class Main {
     public static void main(String[] args) {
 
@@ -8,5 +7,3 @@ public class Main {
             }
         }
 
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
