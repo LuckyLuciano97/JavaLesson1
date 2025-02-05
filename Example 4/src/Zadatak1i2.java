2bbd816d37354ffb7da50798c2cb5fdce40c5da2
LuckyLuciano97 <luka.djogas@gmail.com>
2025-02-05 21:12:07 +0100

    Zadatak 2 dodan

--- Example 4/src/Zadatak1i2.java
+++ Example 4/src/Zadatak1i2.java
@@ -7,9 +7,9 @@ public class Zadatak1i2 {
         //Zadatak 1
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter sentence");
+        System.out.println("Unesi rečenicu");
 
-        String input = sc.nextLine();
+        String input = sc.nextLine().trim();
         int letterCounter = 0;
         int numberCounter = 0;
         int elseCounter = 0;
@@ -27,6 +27,21 @@ public class Zadatak1i2 {
         System.out.println("Slova: " + letterCounter);
         System.out.println("Brojevi: " + numberCounter);
         System.out.println("Ostalo: " + elseCounter);
+
+
+
+        //Zadatak 2
+
+        System.out.println("Unesi rečenicu kojoj želiš okrenuti redoslijed");
+        String normalno = sc.nextLine().trim();
+        String [] rijeci = normalno.split(" ");
+
+        for (int i = rijeci.length-1; i > -1; i--){
+            System.out.print(rijeci[i] + " ");
+        }
+
+
+
 
     }
 
